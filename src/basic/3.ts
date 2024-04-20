ea579e9bb223c954a8e65b17eb82d68bc27a41af
sno0wi <sno0wi20@gmail.com>
2024-04-20 09:54:52 +0300

    3 done

---
+++ src/basic/3.ts
@@ -0,0 +1,14 @@
+type StringOrNumber = string | number;
+
+let test: StringOrNumber;
+
+test = 1;
+test = 'Max';
+
+type EnableOrdisable = 'enable' | 'disable';
+
+let test_2: EnableOrdisable;
+
+test_2 = 'enable';
+test_2 = 'disable';
+
