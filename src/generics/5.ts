eb9d6b38d784c831ef3d58f436c27f3b929b24d2
sno0wi <sno0wi20@gmail.com>
2024-04-20 11:40:22 +0300

    5Gen done

---
+++ src/generics/5.ts
@@ -0,0 +1,12 @@
+export enum UserRole {
+ admin = 'admin',
+ editor = 'editor',
+ guest = 'guest',
+}
+
+
+const RoleDescription:Record<UserRole,string> = {
+ admin: 'Admin User',
+ editor: 'Editor User',
+ guest: 'Guest User',
+};
